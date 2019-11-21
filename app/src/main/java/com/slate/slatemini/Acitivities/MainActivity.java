73530a9b9f6ab7243879e1b7075e6bece17b32b3
Elson Jose <elsonjoseofficial@gmail.com>
2019-11-21 06:39:09 +0530

    Added Search

--- app/src/main/java/com/slate/slatemini/Acitivities/MainActivity.java
+++ app/src/main/java/com/slate/slatemini/Acitivities/MainActivity.java
@@ -6,14 +6,20 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -30,6 +36,8 @@ public class MainActivity extends AppCompatActivity {
     private List<Packages> AppPackagesList;
     private PackageManager packageManager;
     private RecyclerView MainRecyclerview;
+    private EditText MainSearchEditText;
+    private boolean isSearching=false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,28 +48,88 @@ public class MainActivity extends AppCompatActivity {
         AppPackagesList.clear();
         packageManager =  getPackageManager();
 
+        MainSearchEditText = findViewById(R.id.main_search_edittext);
+
         MainRecyclerview = findViewById(R.id.main_recyclerview);
         MainRecyclerview.setHasFixedSize(true);
         DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
         float dpWidth = displayMetrics.widthPixels / displayMetrics.density;
         MainRecyclerview.setLayoutManager(new GridLayoutManager(this,(int) (dpWidth / 80)));
 
-        Intent mainIntent = new Intent(Intent.ACTION_MAIN,null);
-        mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
-        List<ResolveInfo> paclist = packageManager.queryIntentActivities(mainIntent,0);
-        for(int i=0;i<paclist.size();i++)
+        SetMenuItem("");
+
+        MainSearchEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+
+                SetMenuItem(s.toString());
+
+            }
+        });
+
+    }
+
+    private void SetMenuItem(String searched_string)
+    {
+        List<Packages> AppList = new ArrayList<>();
+
+        if(!TextUtils.isEmpty(searched_string))
         {
-            AppPackagesList.add(new Packages(paclist.get(i).activityInfo.packageName,paclist.get(i).loadLabel(packageManager).toString(),paclist.get(i).loadIcon(packageManager)));
+            isSearching = true;
+            Intent mainIntent = new Intent(Intent.ACTION_MAIN,null);
+            mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+            List<ResolveInfo> paclist = packageManager.queryIntentActivities(mainIntent,0);
+            for(int i=0;i<paclist.size();i++)
+            {
+                if(paclist.get(i).loadLabel(packageManager).toString().toLowerCase().contains(searched_string.toLowerCase()))
+                {
+                    AppList.add(new Packages(paclist.get(i).activityInfo.packageName,paclist.get(i).loadLabel(packageManager).toString(),paclist.get(i).loadIcon(packageManager)));
+
+                }
+            }
+
+            MainPackagesAdapter mainPackagesAdapter = new MainPackagesAdapter(AppList);
+            MainRecyclerview.setAdapter(mainPackagesAdapter);
         }
+        else
+        {
+            final InputMethodManager manager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            manager.hideSoftInputFromWindow(MainSearchEditText.getWindowToken(),0);
 
-        MainPackagesAdapter mainPackagesAdapter = new MainPackagesAdapter();
-        MainRecyclerview.setAdapter(mainPackagesAdapter);
 
+            isSearching  = false;
+            Intent mainIntent = new Intent(Intent.ACTION_MAIN,null);
+            mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+            List<ResolveInfo> paclist = packageManager.queryIntentActivities(mainIntent,0);
+            for(int i=0;i<paclist.size();i++)
+            {
+                AppList.add(new Packages(paclist.get(i).activityInfo.packageName,paclist.get(i).loadLabel(packageManager).toString(),paclist.get(i).loadIcon(packageManager)));
+            }
 
+            MainPackagesAdapter mainPackagesAdapter = new MainPackagesAdapter(AppList);
+            MainRecyclerview.setAdapter(mainPackagesAdapter);
+        }
     }
 
     public class MainPackagesAdapter extends RecyclerView.Adapter<MainPackagesAdapter.MainPackagesViewHolder>
     {
+
+        List<Packages> AppPackagesList;
+
+        public MainPackagesAdapter(List<Packages> appPackagesList) {
+            AppPackagesList = appPackagesList;
+        }
 
         @NonNull
         @Override
@@ -112,7 +180,16 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
-        overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out);
-        finish();
+       if(isSearching)
+       {
+           isSearching = false;
+           SetMenuItem("");
+
+       }
+       else
+       {
+           overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out);
+           finish();
+       }
     }
 }
